9eb1306ed7e0e8948701b6826d230ae3b9e96a05
AleAro21 <A01782691@tec.mx>
2023-10-31 17:04:37 -0600

    function to read string from file

--- entrega_integradora1/main.cpp
+++ entrega_integradora1/main.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+
+using namespace std;
+
+// Función para leer el contenido de un archivo en una cadena
+string readFile(const string& filename) {
+    ifstream file(filename);
+    string content, line;
+    while (getline(file, line)) {
+        content += line;
+    }
+    file.close();
+    return content;
+}
+
 
 int main() {
   std::cout << "Hello World!" << std::endl;
